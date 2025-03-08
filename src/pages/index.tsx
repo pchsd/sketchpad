f1640d41c29034632499507615aed10b72a36cf8
pchsd <92434544+pchsd@users.noreply.github.com>
2025-03-08 19:08:57 +0800

    Update index.tsx to remove unused types

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google"
 import { useEffect, useRef, useState } from "react"
 import { diffChars } from 'diff'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
-
 interface Version {
   text: string
   timestamp: Date
